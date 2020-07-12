eb8e13bd4f2323de91e0707ee29387a7e5e849f4
ManLikeGhost <ekpoabasiama@gmail.com>
2020-07-13 00:23:49 +0100

    setting up auth features

--- home/accounts/views.py
+++ home/accounts/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+from django.contrib import messages, auth
+
+
+
 
 def register(request):
     if request.method == 'POST':
-       #Register
-       return
+       first_name = request.POST['first_name']
+       last_name = request.POST['last_name']
+       username = request.POST['username']
+       email = request.POST['email']
+       password = request.POST['password']
+       password2 = request.POST['password2']
+       
+       if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password2, email=email, first_name=first_name, last_name=last_name)
+                    #auto login
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome to home!!')
+                    return redirect('dashboard')
+       else:
+           messages.error(request, 'passwords do not match')
+           return redirect('register')  
+               
     else:   
         return render(request, 'accounts/register.html')
 
@@ -11,12 +40,26 @@ def register(request):
 def login(request): 
     if request.method == 'POST':
         #Login  
-        return    
+        username = request.POST['username']
+        password = request.POST['password']  
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Login complete!! Welcome to home!!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid!!')
+            return redirect('login')
     else:   
         return render(request, 'accounts/login.html')
 
 def logout(request): 
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def dashboard(request):
