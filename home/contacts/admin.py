e694394b44cfdf27e9c18a68d843ee11527ceed8
ManLikeGhost <ekpoabasiama@gmail.com>
2020-07-13 01:16:25 +0100

    admin section modified

--- home/contacts/admin.py
+++ home/contacts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contacts
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_feilds = ('name', 'email', 'listing')
+    list_per_page = 10
+
+admin.site.register(Contacts, ContactAdmin)
