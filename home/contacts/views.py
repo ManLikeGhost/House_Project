c6fd14ca5f8d74d04a00469aa8e05a5b0a126d12
ManLikeGhost <ekpoabasiama@gmail.com>
2020-07-13 02:21:16 +0100

    dashboad setup

--- home/contacts/views.py
+++ home/contacts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contacts
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        
+        #if user had made inquiry before now
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contacts.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+        contact = Contacts(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, messages=message, user_id=user_id)
+        
+        contact.save()
+        
+        message.success(request, 'Your request has been submitted, and a realtor will get back to you!')
+        return redirect('/listings/'+listing_id)
